2cc870ab9a75dc430a1ecb0c7c610f8ff708a0eb
jchrszcz <jchrabaszcz@gmail.com>
2015-04-06 08:07:53 -0400

    fixed init

--- Random_effects/initLesson.R
+++ Random_effects/initLesson.R
@@ -3,6 +3,26 @@
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
 
+find_course <- function(course){
+  file.path(find.package("swirl"), "Courses", gsub(" ", "_", course))
+}
+
+# attempt to display a swirl file
+display_swirl_file <- function(filename, course, lesson=""){
+  fname <- filename
+  if(lesson != "")fname <- file.path(lesson, filename)
+  loc <- gsub(" ", "_", file.path( find_course(course),  fname))
+  toloc <- file.path("swirl_temp", filename)
+  if(!file.exists("swirl_temp"))dir.create("swirl_temp")
+  file.copy(loc, "swirl_temp", overwrite=TRUE)
+  if(isTRUE(1 == grep("*[.]R$", filename))){
+    file.edit(toloc, title=filename)
+  } else {
+    file.show(toloc, title=filename)
+  }
+  message(paste("(Copied file", filename, "to", file.path(getwd(), toloc), ")."))
+}
+
 data(ChickWeight)
 ChickWeight$time <- ChickWeight$Time
 ChickWeight$Time <- as.factor(ChickWeight$time)
