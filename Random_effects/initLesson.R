67db08fcb40c66ab4cf1b3fe2fe9ad162f7546cc
jchrszcz <jchrabaszcz@gmail.com>
2015-04-06 07:14:10 -0400

    added random effects

---
+++ Random_effects/initLesson.R
@@ -0,0 +1,9 @@
+# Code placed in this file fill be executed every time the
+# lesson is started. Any variables created here will show up in
+# the user's working directory and thus be accessible to them
+# throughout the lesson.
+
+data(ChickWeight)
+ChickWeight$Time <- as.factor(ChickWeight$time)
+fixed <- lm(weight ~ Time * Diet, data = ChickWeight)
+random <- aov(weight ~ Diet + Time + Error(Diet:Time), data = ChickWeight)
